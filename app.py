589c75ec51264fd8d049d2aed205b37898913ac6
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-08-13 16:34:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,37 @@
-from llama_cpp import Llama
-from huggingface_hub import hf_hub_download
 import os
+from huggingface_hub import hf_hub_download
+from vllm import LLM, SamplingParams
+
 
 class InferlessPythonModel:
-    def initialize(self):
+    def initialize(self):        
         nfs_volume = os.getenv("NFS_VOLUME")
-        if os.path.exists(nfs_volume + "/llama-2-7b-chat.Q8_0.gguf") == False :
+        if os.path.exists(nfs_volume + "/tinyllama-1.1b-chat-v1.0.Q4_0.gguf") == False :
             cache_file = hf_hub_download(
-                                repo_id="TheBloke/Llama-2-7B-Chat-GGUF",
-                                filename="llama-2-7b-chat.Q8_0.gguf",
+                                repo_id="TheBloke/TinyLlama-1.1B-Chat-v1.0-GGUF",
+                                filename="tinyllama-1.1b-chat-v1.0.Q4_0.gguf",
                                 local_dir=nfs_volume)
-        self.llm = Llama(
-            model_path=f"{nfs_volume}/llama-2-7b-chat.Q8_0.gguf",
-            main_gpu=0,
-            n_gpu_layers=-1)
-
+        self.llm = LLM(model=f"{nfs_volume}/tinyllama-1.1b-chat-v1.0.Q4_0.gguf",
+                  tokenizer="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
+                  gpu_memory_utilization=0.95)
+        
     def infer(self, inputs):
         prompt = inputs["prompt"]
         system_prompt = inputs.get("system_prompt","You are a friendly bot.")
         temperature = inputs.get("temperature",0.7)
         top_p = inputs.get("top_p",0.1)
         top_k = inputs.get("top_k",40)
-        repeat_penalty = inputs.get("repeat_penalty",1.18)
+        repetition_penalty = inputs.get("repetition_penalty",1.18)
         max_tokens = inputs.get("max_tokens",256)
         
-        output = self.llm.create_chat_completion(
-                    messages = [
-                      {"role": "system", "content": f"{system_prompt}"},
-                      {"role": "user","content": f"{prompt}"}],
-                    temperature=temperature, top_p=top_p, top_k=top_k,repeat_penalty=repeat_penalty,max_tokens=max_tokens
-        )
-        text_result = output['choices'][0]['message']['content']
-        
-        return {'generated_result': text_result}
-        
+        CHAT_TEMPLATE = "<|system|>\n{system_prompt}</s>\n<|user|>\n{prompt}</s>\n<|assistant|>\n"  # noqa: E501
+
+        prompts = [CHAT_TEMPLATE.format(system_prompt=system_prompt, prompt=prompt)]
+        sampling_params = SamplingParams(temperature=temperature,top_p=top_p,repetition_penalty=repetition_penalty,
+                                 top_k=top_k,max_tokens=max_tokens)
+        result = self.llm.generate(prompts, sampling_params)
+        result_output = [output.outputs[0].text for output in result]
+
+        return {'result': result_output[0]}        
     def finalize(self):
         self.llm = None
